b801abe229d391ca174f37f81cf54b6239f51eaa
Tauno Põldoja <Tauno.Poldoja>
2017-10-23 15:53:29 +0300

    muudatused

---
+++ src/01-exercises/exercises/09-/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] excuses = new[] { "koer sõi", "Ema ei luba arvutit", " lammas all nurgas" };
+
+            for (int i = 0; i < excuses.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {excuses[i]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
