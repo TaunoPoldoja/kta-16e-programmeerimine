e7dd0d1e67d2360170fa72d7c934c7555a0fd561
Tauno Põldoja <Tauno.Poldoja>
2017-10-23 14:15:30 +0300

    lisatud exercise 02

---
+++ src/01-exercises/exercises/02-exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere");
+            Console.WriteLine("Minu nimi on Tauno ning õpin esimest korda programmeerima!");
+            Console.WriteLine("Kindlasti on võimalik midagi rohkemat, kui lihtsalt teksti printimine");
+            Console.WriteLine("Press any key to continue");
+
+            Console.ReadLine();
+        }
+    }
+}
